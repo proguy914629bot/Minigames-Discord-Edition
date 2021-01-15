9e1bd0431b69ff6e9846bae592b8111f9628bd4f
proguy914629bot <74696067+proguy914629bot@users.noreply.github.com>
2021-01-15 09:27:26 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,33 @@
+import setuptools
 
+requirements = []
+with open("requirements.txt", "r") as fp:
+  requirements = fp.read().splitlines()
+
+longDesc = ""
+with open("README.md", "r") as f:
+  longDesc = f.read()
+
+setuptools.setup = (
+  name = "Minigames-Discord",
+  version = "...", #Whyfai, Do this part
+  author = [
+    "Whyfai",
+    "proguy914629"
+  ],
+  description = "...", #Whyfai
+  long_description = longDesc,
+  url = "https://github.com/proguy914629bot/Minigames-Discord-Edition",
+  packages=setuptools.find_packages(),
+  install_requires = requirements,
+  classifiers = [
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ]
+)
